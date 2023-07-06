4fa095541046342370868021e196df7023c67224
Mahak Agarwal <mahakagarwal248@gmail.com>
2023-07-06 20:15:48 +0530

    api end point updated

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5000'});
+// const API = axios.create({baseURL: 'http://localhost:5000'});
+const API = axios.create({baseURL: 'https://fair-teal-mussel-wrap.cyclic.app/'});
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('Profile')){
